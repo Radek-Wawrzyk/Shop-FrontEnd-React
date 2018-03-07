b4eed2cf65ac287e1cd541c0c468c07dde7a67d3
Radek <radek511@op.pl>
2018-03-07 20:09:00 +0100

    Added part of slider view

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,29 @@
 //libraries
 import React from 'react';
+import './home.css';
 
-const home = (props) => (<h1>To jest Home Page</h1>)
+const home = (props) => (
+<main>
+  <section className="slider">
+    <ul className="slider-list">
+      <li className="slider-item">
+        <div className="slider-item-content">
+          <h1 className="slider-item-title">Rolex aka "zielone szkiełko"</h1>
+          <p className="slider-item-description">Zegarek z najwyższej jakości materiałów nie dla biedaków</p>
+          <div className="item">
+            <button className="btn">Kup teraz</button>
+            <p>10.000 zł</p>
+          </div>
+        </div>
+      </li> 
+    </ul>
+    <ul className="slider-dots">
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
+  </section>
+</main>
+)
 
 export default home;
