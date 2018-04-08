fdc38f2ff6139d79fa93fbe85a49bdc4ec3b2d94
mpyrkosz88 <mpyrkosz88@gmail.com>
2018-04-08 15:59:45 +0200

    up

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,23 +11,23 @@ const navigation = () => (
     <Logo />
     <ul className={classes.NavigationMenu}>
       <NavItem
-        link="/"
+        link={`${process.env.PUBLIC_URL}/`}
         exact
         active={classes.Active}>
         Strona główna
       </NavItem>
       <NavItem
-        link="/products"
+        link={`${process.env.PUBLIC_URL}/products`}
         active={classes.Active}>
         Oferta
       </NavItem>
       <NavItem
-        link="/about"
+        link={`${process.env.PUBLIC_URL}/about`} 
         active={classes.Active}>
         O nas
       </NavItem>
       <NavItem
-        link="/contact"
+        link={`${process.env.PUBLIC_URL}/kontakt`}
         active={classes.Active}>
        Kontakt
       </NavItem>
